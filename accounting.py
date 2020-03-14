ce2c38822137b544605a9cba27195035b0bb1d11
apolot99 <44310628+apolot99@users.noreply.github.com>
2020-03-14 14:02:13 -0400

    Create accounting.py
    new file for acccounting summaries

---
+++ accounting.py
@@ -0,0 +1,11 @@
+# An app for Accounting summaries
+# Created by Gloria Apolot
+# Created on March 14th, 2020
+
+# This file is located in this directory
+# C:\Users\Apolot\Your team Dropbox\Gloria Apolot\Gloria\Apps
+
+# First import a package (an add-on) called streamlit
+from streamlit import title
+from streamlit import subheader
+from streamlit import write
