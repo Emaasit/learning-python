f75d2d24d1f17dfc7df0d50fdb93c3da23f3760f
apolot99 <44310628+apolot99@users.noreply.github.com>
2020-03-14 14:15:01 -0400

    Update accounting.py
    added tables and more text

--- accounting.py
+++ accounting.py
@@ -9,3 +9,54 @@
 from streamlit import title
 from streamlit import subheader
 from streamlit import write
+
+# Then use streamlit to create a title for the App
+title("PRINCIPLES OF ACCOUNTING 2")
+
+# Then use streamlit to create a subheader for the App
+subheader("Summary: Principles of Accounting 2")
+
+# Then use streamlit to write our task for the App
+write("This is a summary of topics covered in Principles of Accounting 2. This course is designed to give a student the basic ability to prepare, analyze and interpret accounting data for managerial decision making.")
+write(
+	"""
+	Let's start by Comparing financial & managerial accounting. 
+
+|	 Financial                                               |   Managerial  |      
+| -------------                                              |:-------------:| 
+| -Users; internal officers of a company like managers       | -Users; external officers of a company like stockholders, creditors | 
+| -Produce financial statements quaterly and anually         | -Produce internal reports frequently     |  
+| -Reports are for general purpose                           | -Reports are for special purpose for specific decisions    |    
+| -Financial statements have to be audited by CPA             | -No independent audits |  
+
+
+----
+	"""
+	)
+
+
+write(
+	"""
+	Next let's look at managerial cost concepts.
+
+	   Manufacturing costs- consists of activities & processes that convert raw materials into finished goods. Manufacturing costs are classified as direct materials, direct labour and manufacturing overhead.
+
+	"""
+	) 
+
+write(
+	"""
+ 
+     a) Direct materials
+
+-Raw materials are the basic materials & parts used in the manufacturing process.
+-Raw materials that can be physically and directly associated with the finished product during the manufacturing process are direct materials e.g flour in the baking of bread, steel in the making of automobiles.
+
+-Materials that can not be easily associated with the finished product are called indirect materials.
+
+Characteristics 
+
+
+
+    """
+	)
