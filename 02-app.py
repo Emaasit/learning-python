85900ab0bd4185365335c0d5906b90a8de9a1684
apolot99 <44310628+apolot99@users.noreply.github.com>
2020-03-14 14:52:05 -0400

    Update 02-app.py
    Copied from sublime text editor

--- 02-app.py
+++ 02-app.py
@@ -3,3 +3,19 @@
 
 # This file is located in this directory
 # C:\Users\Apolot\Your team Dropbox\Gloria Apolot\Gloria\Apps
+
+# First import a package (an add-on) called streamlit
+from streamlit import title
+from streamlit import subheader
+from streamlit import write
+from streamlit import image
+from streamlit import markdown
+from streamlit import info
+from streamlit import warning
+from streamlit import success
+
+# Then use streamlit to create a title of the App
+title("My favourite books")
+
+# Then use streamlit to create a subheader of the App
+subheader("Favourite romance books")
